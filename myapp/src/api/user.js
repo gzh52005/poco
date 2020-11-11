037afd7858151608d90fdd7dab3f6017bc4ad8bf
18819458629 <757175148@qq.com>
2020-11-11 22:52:28 +0800

    用户接口

--- myapp/src/api/user.js
+++ myapp/src/api/user.js
@@ -1,1 +1,44 @@
-// 请求用户名接口
+let request = require('../utils/request');
+
+//功能：验证用户名是否存在
+function checkName(username) {
+    return request.get('/user/checkname',{
+        params : {
+            username 
+        }
+    })
+}
+
+//功能：注册功能
+function reg(username,password) {
+    return request.post('/user/reg',{
+        username,
+        password
+    })
+}
+
+//功能：登录功能
+function login(username,password) {
+    return request.get('/user/login',{
+        params : {
+            username,
+            password
+        }
+    })
+}
+
+//功能：验证token
+function checkToken(token) {
+    return request.get('/user/verify',{
+        params : {
+            token,
+        }
+    })
+}
+
+module.exports = {
+    checkName,
+    reg,
+    login,
+    checkToken
+}
--- myapp/src/views/Login/index.jsx
+++ myapp/src/views/Login/index.jsx
@@ -1,7 +1,15 @@
 // 登录
-import React from 'react'
+import React, {useState, useEffect } from 'react';
+import cartApi from "../api/cartApi";
 
 function Login() {
+    useEffect(function () {//useEffect 里面传入的参数是一个函数
+        // 这里的代码只有初始化时执行,只需要在初始化时执行一次的操作可以在这里做
+        cartApi.reviewList().then(res => {
+          console.log(res.data.data, 77);
+          // console.log(this.arr);
+        });       
+       }, [])
     return (
         <div>Login</div>
     )
