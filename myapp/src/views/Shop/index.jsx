7612e6f49c9f2b250826c777465d9e1fc31f52d8
penghuinb <941097933@qq.com>
2020-11-12 14:02:03 +0800

    提交2

--- myapp/src/views/Shop/index.jsx
+++ myapp/src/views/Shop/index.jsx
@@ -1,11 +1,72 @@
 // POCO认证
 import React from 'react'
-
-function Poco() {
+import './index.scss';
+function Shop() {
     return (
-        <div>Shop</div>
+        <div className="shop-bigBox">
+            <div className="shop-box">
+                <i className='shop-logo'></i>
+                <i className='shop-nav'></i>
+            </div>
+            <div className="shop-box1">
+                <p></p>
+                <div>
+                    <ul>
+                        <li>
+                            <img src="https://www.woimg.com/beego/86ad6257273da31a0beb528ce1993177.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="https://www.woimg.com/beego/86ad6257273da31a0beb528ce1993177.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="https://www.woimg.com/beego/86ad6257273da31a0beb528ce1993177.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="https://www.woimg.com/beego/86ad6257273da31a0beb528ce1993177.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="https://www.woimg.com/beego/86ad6257273da31a0beb528ce1993177.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="https://www.woimg.com/beego/86ad6257273da31a0beb528ce1993177.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="https://www.woimg.com/beego/86ad6257273da31a0beb528ce1993177.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="https://www.woimg.com/beego/86ad6257273da31a0beb528ce1993177.png" alt="" />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="shop-box2">
+                <p></p>
+                <div>
+                    <ul>
+                        <li>
+                            <img src="https://www.woimg.com/beego/86ad6257273da31a0beb528ce1993177.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="https://www.woimg.com/beego/86ad6257273da31a0beb528ce1993177.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="https://www.woimg.com/beego/86ad6257273da31a0beb528ce1993177.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="https://www.woimg.com/beego/86ad6257273da31a0beb528ce1993177.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="https://www.woimg.com/beego/86ad6257273da31a0beb528ce1993177.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="https://www.woimg.com/beego/86ad6257273da31a0beb528ce1993177.png" alt="" />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
 
 
-export default Poco;
+export default Shop;
